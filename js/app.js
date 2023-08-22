b1b1cc84dc358449cd8eac26be19472aa0f1f016
Abner <contato.abnerbolandim@gmail.com>
2023-08-22 14:59:22 -0300

    Configura sendForm para dSuccess e Error

---
+++ js/app.js
@@ -0,0 +1,58 @@
+class FormSubmit{
+    constructor(settings){
+        this.settings = settings;
+        this.form = document.querySelector(settings.form);
+        this.formButton = document.querySelector(settings.button);
+        if(this.form){
+            this.url = this.form.getAttribute("action");
+
+        }
+    }
+
+    displaySuccess(){
+        this.form.innerHTML = this.settings.success;
+    }
+
+    displayError(){
+        this.form.innerHTML = this.settings.error;
+    }
+
+    getFormObject(){
+        const formObject = {};
+        const fields = this.form.querySelectorAll("[name]");
+
+        fields.forEach((field) => {
+            formObject[field.getAttribute("name")] = field.value;
+        });
+    }
+
+    async sendForm(){
+        try{
+            await fetch(this.url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                body: "",
+            });
+            this.displaySuccess();
+        } catch (error){
+            this.displayError();
+            throw new Error(error);
+        }
+    }
+
+    init(){
+        if(this.form) this.formButton.addEventListener("click", () => this.displaySuccess());
+        return this;
+    }
+}
+
+const FormSubmit = new FormSubmit({
+    form: "[data-form]",
+    button: "[data-button]",
+
+    successe: "<h1 class='success'>Mensagem enviada</h1>",
+    error:"<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
+});
